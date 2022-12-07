59328b44e12dbce7dc8d30b4689e69915349b8c1
Reed Keyes <rbkeyes@gmail.com>
2022-12-06 20:53:06 -0800

    added code to append items to dropdown

--- assets/js/multi-select-dropdown.js
+++ assets/js/multi-select-dropdown.js
@@ -2,3 +2,13 @@ var dropdown = $('.dropdown-pane');
 console.log("dropdown: " + dropdown);
 
 dropdown.attr('style', 'width: 75vw')
+
+var input = $('#input');
+var checkbox = $('<input>');
+checkbox.attr('type', "checkbox");
+checkbox.attr('id', 'checkbox4');
+var label = $('<label>');
+label.attr('for', 'checkbox4');
+label.text('Checkbox 4')
+input.append(checkbox);
+input.append(label);
