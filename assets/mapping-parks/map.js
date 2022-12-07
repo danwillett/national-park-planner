9a10de1244874fca2538395c997fc547fdbddace
danwill <daniel.willett2@gmail.com>
2022-12-06 21:35:02 -0800

    added park modals to the map

--- assets/mapping-parks/map.js
+++ assets/mapping-parks/map.js
@@ -13,6 +13,11 @@ function makeFeatures(object) {
     console.log(i)
 
     var parkName = object.data[i].fullName;
+    // var parkCode = object.data[i].parkCode;
+    var parkImage = object.data[i].images[0].url;
+    var parkDescription = object.data[i].description;
+    var parkLocation = object.data[i].addresses[0].city + ', ' + object.data[i].addresses[0].stateCode;
+    console.log(parkImage)
     parkNames.push(parkName)
     console.log(parkName)
 
@@ -24,7 +29,12 @@ function makeFeatures(object) {
     var addFeature = new ol.Feature({
       geometry: new ol.geom.Point(ol.proj.fromLonLat([longitude, latitude])),
       name: parkName,
-      url: object.data[i].url
+      // code: parkCode,
+      location: parkLocation,
+      image: parkImage,
+      url: object.data[i].url,
+      description: parkDescription
+      
     });
 
     parkPins[parkName] = addFeature
@@ -48,25 +58,46 @@ function makeFeatures(object) {
   })
 
   map.addLayer(pins)
+  
+  // map.setAttribute('data-open', 'park-preview');
 
+  
   map.getViewport().addEventListener("click", function (e) {
     map.forEachFeatureAtPixel(map.getEventPixel(e), function (feature, layer) {
       console.log(feature)
+      
+      // begins filling in preview code block
+      var previewEl = document.getElementById('park-preview');
+
+      var parkTitleEl = document.getElementById('park-title');
+      parkTitleEl.textContent = feature.values_.name;
+
+      var locationEl = document.getElementById('location');
+      locationEl.textContent = feature.values_.location;
+
+      var backgroundImageEl = document.getElementById('park-image');
+      backgroundImageEl.setAttribute('class', 'park-image')
+      backgroundImageEl.style.backgroundImage = 'url(' + feature.values_.image; + ')'
+
+      var parkDescriptionEl =  document.getElementById('park-description');
+      parkDescriptionEl.textContent = feature.values_.description;
+      
+      var parkWebsiteLinkEl = document.getElementById('park-website');
       var link = feature.values_.url;
-      window.open(link, '_blank')
-      //do something
+      parkWebsiteLinkEl.setAttribute('href', link)
+      parkWebsiteLinkEl.setAttribute('target', '_blank')
+      parkWebsiteLinkEl.textContent = 'Learn More!'
+      
+
+      $(previewEl).foundation('open')
     });
   });
 
 }
 
-// fetch('https://www.nps.gov/acad/index.htm')
-// .then(function(result){
-//   console.log(result)
-// })
-// .then(function(data){
-//   console.log(data)
-// })
+function makePreviews() {
+
+}
 
 var map = new ol.Map({
   layers: [
