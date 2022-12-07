56e8744351f17db9375331094923b04226fb2ecd
Reed Keyes <rbkeyes@gmail.com>
2022-12-07 11:20:48 -0800

    added intra-page links to footer nav

--- assets/js/multi-select-dropdown.js
+++ assets/js/multi-select-dropdown.js
@@ -3,14 +3,17 @@ console.log("dropdown: " + dropdown);
 
 dropdown.attr('style', 'width: 75vw')
 
+// create list item elements for dropdown
+var activity1 =$('#activity1')
+var listItem = $('<li>')
 var input = $('#input');
-var dropdownList =$('<grid-x>')
-console.log(dropdownList);
 var checkbox = $('<input>');
 checkbox.attr('type', "checkbox");
 checkbox.attr('id', 'checkbox4');
 var label = $('<label>');
 label.attr('for', 'checkbox4');
 label.text('Checkbox 4')
-input.append(checkbox);
-input.append(label);
+listItem.append(checkbox);
+listItem.append(label);
+console.log(listItem.text());
+activity1.append(listItem)
