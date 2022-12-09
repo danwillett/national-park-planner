1c143aea94426b0c635f3b47fcef7bfc4989273e
danwill <daniel.willett2@gmail.com>
2022-12-09 11:08:26 -0800

    added remove button to saved park list

--- assets/mapping-parks/map.js
+++ assets/mapping-parks/map.js
@@ -1,6 +1,5 @@
-// this function adds pin points for the filtered park locations to the map
-// this function adds pin points for the filtered park locations to the map
 
+// creates global variable for parks saved in local storage and then loads them if any are saved
 if (localStorage.getItem("savedParks") === null) {
   var savedParks = {};
 } else {
@@ -8,74 +7,117 @@ if (localStorage.getItem("savedParks") === null) {
   loadSavedSearches(savedParks)
 }
 
-
-console.log(document.getElementById("hey"))
-function loadSavedSearches(parksToLoad){
+// function creates info panels for each saved park underneath the map
+function loadSavedSearches(parksToLoad) {
 
   var savedParksEl = document.getElementById('saved-parks');
   var savedKeys = Object.keys(parksToLoad)
   console.log(savedParksEl)
 
-  for (var i = 0; i < savedKeys.length; i++){
+  for (var i = 0; i < savedKeys.length; i++) {
 
     var parkDetails = parksToLoad[savedKeys[i]]
-    if (document.getElementById(parkDetails.name)===null) {
+    if (document.getElementById(parkDetails.name) === null) {
       var parkTitle = parkDetails.name;
-    var cardEl = $('<div>')
-    $(cardEl).attr("class", "card");
-    $(cardEl).css("width", "300px");
-    $(cardEl).attr("id", parkDetails.name)
-    
 
-    var titleEl = $('<div>')
-    $(titleEl).attr("class", "card-divider")
-    $(titleEl).text(parkTitle)
-    $(cardEl).append(titleEl)
+      var cardEl = $('<div>')
+      $(cardEl).attr("class", "card");
+      $(cardEl).css("width", "300px");
+      $(cardEl).attr("id", parkDetails.name)
 
-    console.log(parksToLoad.image)
+      var headEl = $('<div>')
+      $(headEl).attr("class", "card-divider")
+      
+      var parkTitleEl = $('<h4>');
+      $(parkTitleEl).text(parkTitle)
+      $(headEl).append(parkTitleEl)
 
-    var imgEl = $('<img>');
+      $(cardEl).append(headEl)
+
+      console.log(parksToLoad.image)
+
+      var imgEl = $('<img>');
       $(imgEl).attr("src", parkDetails.image)
       $(imgEl).attr("class", "savedImages");
 
-    $(cardEl).append(imgEl)
+      $(cardEl).append(imgEl)
 
-    var infoEl = $('<div>')
-    $(infoEl).attr("class", "card-section")
+      var infoEl = $('<div>')
+      $(infoEl).attr("class", "card-section")
 
-    var locEl = $('<h4>')
-    $(locEl).text(parkDetails.location)
-    $(infoEl).append(locEl)
 
-    var pEl = $('<p>')
-    $(pEl).text(parkDetails.description)
-    $(infoEl).append(pEl)
-    $(cardEl).append(infoEl)
-    
-    var linkEl = $('<a>');
-    linkEl.text("Check it out!")
-    linkEl.attr("href", parkDetails.url)
-    linkEl.attr("target", "_blank")
-    $(cardEl).append(linkEl)
+      var locEl = $('<div>')
+      $(locEl).css({ "display": "flex", "flex-direction": "row", "flex-wrap": "wrap", "justify-content": "space-between", "align-items":"center" })
 
-    var removeButton = $('<button>')
-    $(removeButton).text("Remove Park")
-    console.log(removeButton)
-    $(cardEl).append(removeButton)
+      var locName = $('<h5>')
+      $(locName).text(parkDetails.location)
+      $(locEl).append(locName)
 
-    var addPinButton = $('<button>')
-    $(addPinButton).text("See on map")
-    console.log(addPinButton)
-    $(cardEl).append(addPinButton)
+      var pinButton = $('<button>');
+      $(pinButton).text("Pin to Map")
+      $(pinButton).attr("class", "button small pin-button")
+      $(locEl).append(pinButton);
+      $(infoEl).append(locEl)
 
-    $(savedParksEl).append(cardEl)
+      var pEl = $('<p>')
+      $(pEl).text(parkDetails.description)
+      $(infoEl).append(pEl)
+
+      var navEl = $('<nav>')
+      $(navEl).css({ "display": "flex", "flex-direction": "row", "flex-wrap": "wrap", "justify-content": "space-between" })
+
+      var linkEl = $('<a>');
+      linkEl.text("Check it out!")
+      linkEl.attr("href", parkDetails.url)
+      linkEl.attr("target", "_blank")
+      linkEl.attr("class", "button")
+
+      $(navEl).append(linkEl)
+
+      var removeButton = $('<button>')
+      $(removeButton).text("Remove")
+      $(removeButton).attr("class", "alert button remove-button")
+
+      $(navEl).append(removeButton)
+
+      $(infoEl).append(navEl)
+
+      $(cardEl).append(infoEl)
+
+      $(savedParksEl).append(cardEl)
     } else {
       console.log("park already saved")
     }
-    
+
   }
+
+  // when remove button is clicked, the saved park display will be removed and deleted from local storage
+  $('.remove-button').on('click', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    console.log($(this).parent())
+    var cardContainer = $(this).parent().parent().parent()
+    var parentName = cardContainer.attr("id")
+    $(cardContainer).remove()
+
+    delete savedParks[parentName];
+    localStorage.setItem("savedParks", JSON.stringify(savedParks))
+    savedParks = JSON.parse(localStorage.getItem("savedParks"));
+
+  })
+
+  $('.pin-button').on('click', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    console.log("hey")
+  })
+
 }
 
+
+
+// this function adds pin points for the filtered park locations to the map
 function makeFeatures(object) {
   console.log(object.data.length)
   var parkPins = {};
@@ -115,7 +157,7 @@ function makeFeatures(object) {
   }
 
   // combines map features into a single vector layer that will be added to the map
-  // combines map features into a single vector layer that will be added to the map
+
   var pins = new ol.layer.Vector({
     source: new ol.source.Vector({
       features: Object.values(parkPins)
@@ -134,24 +176,24 @@ function makeFeatures(object) {
   console.log(pins)
   // adds the location pins to the map
   map.addLayer(pins)
-  
+
   // generates a new view to zoom in on the parks
-  var ext=addFeature.getGeometry().getExtent();
-  var center=ol.extent.getCenter(ext);
+  var ext = addFeature.getGeometry().getExtent();
+  var center = ol.extent.getCenter(ext);
 
   console.log(center)
-  map.setView( new ol.View({
-    center: [center[0] , center[1]],//zoom to the center of your feature
+  map.setView(new ol.View({
+    center: [center[0], center[1]],//zoom to the center of your feature
     zoom: 5 //here you define the levelof zoom
   }));
-  
+
   // adds an event listener to the map viewport
   map.getViewport().addEventListener("click", function (e) {
     // when pixels with pins are selected, it will show the selected park's modal preview
     // when pixels with pins are selected, it will show the selected park's modal preview
     map.forEachFeatureAtPixel(map.getEventPixel(e), function (feature, layer) {
       console.log(feature)
-      
+
       // adds content to empty div element containing modal structure
       var previewEl = document.getElementById('park-preview');
 
@@ -173,51 +215,40 @@ function makeFeatures(object) {
       parkWebsiteLinkEl.setAttribute('href', link)
       parkWebsiteLinkEl.setAttribute('target', '_blank')
       parkWebsiteLinkEl.textContent = 'Learn More!'
-      
+
       // call foundation method to open the modal when clicked
       $(previewEl).foundation('open')
 
-        // set park into local storage
-        var saveButton = document.getElementById('save-button')
-        saveButton.addEventListener('click', function(event){
-          
-            event.preventDefault();
-            event.stopPropagation();
+      // set park into local storage
+      var saveButton = document.getElementById('save-button')
+      saveButton.addEventListener('click', function (event) {
 
-            console.log(localStorage.getItem("savedParks"))
-            
-            var code = feature.values_.code;
-            console.log(code)
-            thisPark = feature.values_;
-            // {
-            //   name: feature.values_.name,
-            //   code: feature.values_.code,
-            //   description
-            //   location: feature.values_.location,
-            //   image: feature.values_.image,
-            //   url: feature.values_.url
-            // }
-            console.log(thisPark)
-            console.log(savedParks)
-            savedParks[feature.values_.name] = thisPark
-                     
-            localStorage.setItem("savedParks", JSON.stringify(savedParks))
-            savedParks = JSON.parse(localStorage.getItem("savedParks"));
-            console.log(savedParks)
-      
-            loadSavedSearches(savedParks);
-        }, {once: true})
-        
-        
-      
+        event.preventDefault();
+        event.stopPropagation();
 
+        console.log(localStorage.getItem("savedParks"))
+
+        var code = feature.values_.code;
+        console.log(code)
+        thisPark = feature.values_;
+
+        console.log(thisPark)
+        console.log(savedParks)
+        savedParks[feature.values_.name] = thisPark
+
+        localStorage.setItem("savedParks", JSON.stringify(savedParks))
+        savedParks = JSON.parse(localStorage.getItem("savedParks"));
+        console.log(savedParks)
+
+        loadSavedSearches(savedParks);
+      }, { once: true })
     });
   });
 
 }
 
 // function adds 
-function addParksToMap(parksObject) { 
+function addParksToMap(parksObject) {
 
   // adds variables to be used in fetch call to nps api
   var parkBaseUrl = 'https://@developer.nps.gov/api/v1'
@@ -225,21 +256,21 @@ function addParksToMap(parksObject) {
 
   var selectedParks = [];
   // creating final list of park codes to be used in fetch call
-  for (var pc = 0; pc < parksObject.length; pc++){    
-  selectedParks.push(parksObject[pc].parkCode);
-  } 
+  for (var pc = 0; pc < parksObject.length; pc++) {
+    selectedParks.push(parksObject[pc].parkCode);
+  }
   console.log(selectedParks)
 
   // gets park information from final list of park codes
   fetch(parkBaseUrl + '/parks?parkCode=' + String(selectedParks) + apiParam)
-  .then(function (response) {
-    return response.json();
+    .then(function (response) {
+      return response.json();
 
-  }).then(function (obj) {
-    console.log(obj.data.length);
-    // with information, adds map features based on park location
-    makeFeatures(obj);
-  })
+    }).then(function (obj) {
+      console.log(obj.data.length);
+      // with information, adds map features based on park location
+      makeFeatures(obj);
+    })
 }
 
 var map = new ol.Map({
@@ -254,7 +285,7 @@ var map = new ol.Map({
   target: 'map',
 });
 
-var parkList = [ {park:"yosemite", parkCode:"yose", url:"someurl"}, {park:"yosemite", parkCode:"chis", url:"someurl"}, {park:"yosemite", parkCode:"fopo", url:"someurl"}]
+var parkList = [{ park: "yosemite", parkCode: "yose", url: "someurl" }, { park: "yosemite", parkCode: "chis", url: "someurl" }, { park: "yosemite", parkCode: "fopo", url: "someurl" }]
 
 addParksToMap(parkList)
 
